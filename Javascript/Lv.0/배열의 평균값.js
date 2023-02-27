4fd2a5d1449c8785086393c077682fbda6e047df
DoYoung Park <cym7356@naver.com>
2023-02-28 08:13:19 +0900

    기본 방법과 reduce 사용해서 평균 값 구하기

--- Javascript/Lv.0/배열의 평균값.js
+++ Javascript/Lv.0/배열의 평균값.js
@@ -1,4 +1,14 @@
-function solution(numbers) {    
-    return numbers.reduce((a,c) => a + c , 0) / numbers.length;
+function solution(numbers) {
+    var answer = 0;
+    let sum = 0;
+    for(let i=0; i<numbers.length; i++){
+        sum += numbers[i]; //배열의 요소들을 하나씩 더한다
+    }
+    return answer = sum / numbers.length; //더한 값과 배열의 길이 (=요소 갯수)를 나눈다.
 }
 
+// reduce 사용하기 
+
+function solution(numbers){
+    return numbers.reduce((sum,current) => sum + current) / numbers.length;
+}
