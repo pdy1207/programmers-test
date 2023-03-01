4fda23bd83469bcb1b307a0ad6787bef75c6f32f
DoYoung Park <cym7356@naver.com>
2023-03-02 08:06:05 +0900

    map 함수 중요 !

--- Javascript/Lv.0/배열 두배 만들기.js
+++ Javascript/Lv.0/배열 두배 만들기.js
@@ -5,3 +5,13 @@ function solution(numbers) {
 function solution(numbers) {
     return numbers.map(i=>i*2);
 }
+
+function solution(numbers) {
+    var answer = [];
+    for(let i=0; i<numbers.length; i++){
+        answer[i] = numbers[i] * 2;
+        
+    }
+    console.log(answer);
+    return answer;
+}
