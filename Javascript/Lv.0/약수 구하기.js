aca7ccc57da805dd73d7a9edf248a5e47610bef7
DoYoung Park <cym7356@naver.com>
2023-03-07 07:47:05 +0900

    Create 약수 구하기.js

---
+++ Javascript/Lv.0/약수 구하기.js
@@ -0,0 +1,18 @@
+function solution(n) {
+  let answer = [];
+  for (let i = 1; i < Math.sqrt(n); i++) {
+    if (n % i === 0) answer = [...answer, i, n / i];
+  }
+  if (Number.isInteger(Math.sqrt(n))) answer = [...answer, Math.sqrt(n)];
+  return answer.sort((a, b) => a - b);
+}
+function solution(n) {
+    var answer = [];
+    for(let i = 1; i<= n; i++){
+        if (n%i == 0){
+            answer.push(i);
+        }
+    }
+
+    return answer
+}
