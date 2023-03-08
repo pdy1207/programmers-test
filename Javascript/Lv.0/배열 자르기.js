642488380b94b749748928c6575e3ba7c87472e5
DoYoung Park <cym7356@naver.com>
2023-03-08 09:09:23 +0900

    slice , splice 차이점

--- Javascript/Lv.0/배열 자르기.js
+++ Javascript/Lv.0/배열 자르기.js
@@ -1,3 +1,12 @@
 function solution(numbers, num1, num2) {
   return numbers.slice(num1, num2 + 1);
 }
+function solution(numbers, num1, num2) {    
+    return numbers.splice(num1,num2);
+}
+
+위 문제는 다음과 같다.
+
+splice의 내부를 살펴보자면 splice(start, deleteCount) 로 구성되어있습니다. 
+
+그리고 slice 메소드는 slice(begin, end)로 구성되어있습니다.
