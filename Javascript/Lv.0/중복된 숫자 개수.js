9f4cdd07f8044472e1a37dbd3bab8048c97e383f
DoYoung Park <cym7356@naver.com>
2023-03-13 08:19:39 +0900

    filter 사용법 추가

--- Javascript/Lv.0/중복된 숫자 개수.js
+++ Javascript/Lv.0/중복된 숫자 개수.js
@@ -1,9 +1,18 @@
 function solution(array, n) {
-let newArr = [];
-    for (let i = 0; i < array.length; i++) {
-        if (array[i] === n) {
-    newArr.push(array[i]);
-        }
+    var newArr = [];
+    for(let i=0; i<array.length; i++){
+        if(array[i] === n){
+            newArr.push(array[i])
+        }         
     }
-        return newArr.length;
+     return newArr.length;
+}
+
+
+function solution(array, n) {
+    var answer = 0;
+    let Array = array.filter((item) => item === n)
+    answer = Array.length
+
+    return answer;
 }
