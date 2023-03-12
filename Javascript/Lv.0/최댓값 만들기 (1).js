bf144d3edfcb3857f9243d875227f807e5217767
DoYoung Park <cym7356@naver.com>
2023-03-12 12:39:46 +0900

    조금 더 쉬운 방법 추가

--- Javascript/Lv.0/최댓값 만들기 (1).js
+++ Javascript/Lv.0/최댓값 만들기 (1).js
@@ -2,3 +2,8 @@ function solution(numbers) {
   const [first, second, ...rest] = numbers.sort((a, b) => b - a);
   return first * second;
 }
+
+function solution(numbers) {
+    numbers.sort((a,b)=>b-a);
+    return numbers[0] * numbers[1]
+}
